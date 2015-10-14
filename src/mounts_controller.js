460b08cf73955e8096e9d050ce9daacee9c4c598
Dmitry Boutsko <boutsko@gmail.com>
2015-10-14 08:48:52 +0300

    add ng-repeat iteration

--- src/mounts_controller.js
+++ src/mounts_controller.js
@@ -1,5 +1,11 @@
 var app = angular.module('thSample', []);
 
 app.controller('MountsController', function($scope){
-  $scope.title = "Альпы"
+    $scope.title = "Альпы"
+
+    $scope.mounts = [
+        {title: "Маттерхорн", alt: 4478},
+        {title: "Монблан", alt: 4810}
+    ];
 });
+
